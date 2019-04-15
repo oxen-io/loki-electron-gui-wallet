9c6fe286c0eb7d7087cc1420e1c54491c5eb33fe
Maxim Antonov <helloto@me.com>
2019-04-15 23:33:48 +0300

    Translate to Russian

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,13 +1,16 @@
 import enUS from "./en-us"
+import ru from "./ru"
 
 // ADD LANGUAGES HERE
 const languages = [
-    { name: "English", code: "en-us", flag: "us" }
+    { name: "English", code: "en-us", flag: "us" },
+    { name: "Russian", code: "ru", flag: "ru" }
 ]
 
 export { languages }
 
 // DO NOT MODIFY THIS EXPORT, LANGUAGE FILES CAN BE DYNAMICALLY LOADED
 export default {
-    "en-us": enUS
+    "en-us": enUS,
+    "ru": ru
 }
