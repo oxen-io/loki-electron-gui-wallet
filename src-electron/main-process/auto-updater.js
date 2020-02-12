5197ed5d77bc5049ac704908062035e68908c060
Brian Jian Zhao <jian10au@gmail.com>
2020-02-12 11:10:29 +1100

    ready for merge back to development

--- src-electron/main-process/auto-updater.js
+++ src-electron/main-process/auto-updater.js
@@ -65,7 +65,7 @@ function checkForUpdate (onQuitAndInstall) {
     })
 
 
-    //progressbar is already there and show the progress bar value;
+
     autoUpdater.on("download-progress", progress => {
         progressBar.value = progress.percent
     })
@@ -73,32 +73,23 @@ function checkForUpdate (onQuitAndInstall) {
     autoUpdater.on("update-downloaded", () => {
         console.log("Update downloaded")
         isUpdating = false
-        // again, we need to set the status for this isUpdating back to false, 
-        // actually always set this to true only when the download is in progress but why this is the 
-        // case .
-
+        
         if (progressBar) {
             progressBar.setCompleted()
             progressBar = null
             //indicate the download progress finishes and set the progressBar back to null
-            // does this mean it close the progress bar
         }
 
         // If download and install was selected then quit and install
         if (downloadAndInstall && onQuitAndInstall) {
             onQuitAndInstall(autoUpdater)
             downloadAndInstall = false
-            //not sure why this line of code is needed.
+
         }
     })
 
     autoUpdater.checkForUpdates()
-    //this line of code is self explainatory; check always set the autoUpdater to check for updates.
-    //then all there will be some event that triggers that all the event listener we have set in above.
 }
-
-// the whole file is only a giant function
-
 
 
 export { checkForUpdate }
--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -2,8 +2,6 @@ import { app, ipcMain, BrowserWindow, Menu, dialog } from "electron"
 import { version, productName } from "../../package.json"
 import { Backend } from "./modules/backend"
 import { checkForUpdate } from './auto-updater'
-// looks ok by Brian
-
 import menuTemplate from "./menu"
 import isDev from "electron-is-dev"
 const portscanner = require("portscanner")
@@ -44,9 +42,6 @@ const inputMenu = Menu.buildFromTemplate([
     { role: "selectall" }
 ])
 
-// the above code is to create the menu button based on the template and set
-// the template string
-
 function createWindow () {
     /**
      * Initial window options
@@ -68,26 +63,15 @@ function createWindow () {
         title
     })
 
-    // when a window starts; it has a lot of default GUI setting;
-    // it has things like icon title and etc.
-
-
-    //why does everything starts with close Brian 
     mainWindow.on("close", (e) => {
 
         if(installUpdate) {
             return
-            // by default the installUpdate is set as false but how can we set it to true
         }
-        //how does the above function works
-
 
         if (process.platform === "darwin") {
             if (forceQuit) {
                 forceQuit = false
-                //if running on MacOS and see the forceQuit is true and 
-                // set back the forceQuit to false but why
-                // as the force quit by default is set as false in here anyway
                 if (showConfirmClose) {
                     e.preventDefault()
                     mainWindow.show()
@@ -108,14 +92,9 @@ function createWindow () {
             }
         }
     })
-    //there is a lot of settings above regarding how the windows behave in
-    // the context of the OSX and other OS. I don't fully undersetand so far.
-
-
     ipcMain.on("confirmClose", (e, restart) => {
         showConfirmClose = false
 
-        // In dev mode, this will launch a blank white screen
         if (restart && !isDev) app.relaunch()
 
         if (backend) {
@@ -127,12 +106,10 @@ function createWindow () {
             app.quit()
         }
     })
-    // what happens with devMode? How can run a devMode in electron at and what is the dev mode?
 
 
 
     mainWindow.webContents.on("did-finish-load", () => {
-        // Set the title
         mainWindow.setTitle(title)
 
         require("crypto").randomBytes(64, (err, buffer) => {
@@ -179,14 +156,7 @@ function createWindow () {
 
 app.on("ready", () => {
     console.log(checkForUpdate, 'definitely is run in here')
-    // below part is for checking the auto update; 
-    // I don't understand how come this checkForUpdate functions actually takes in 
     checkForUpdate(
-        // this bits is super confusing;
-        // it basically says we defined an anynoumous function in here;
-        // this anonymous function will then interact on of the app event listener to define the behaviour of 
-        // 
-
         autoUpdater => {
         if (mainWindow) {
             mainWindow.webContents.send("showQuitScreen")
@@ -228,8 +198,6 @@ app.on("before-quit", () => {
     if (installUpdate) {
         return
     }
-
-// again why do we need this line of code?
 
     if (process.platform === "darwin") {
         forceQuit = true
