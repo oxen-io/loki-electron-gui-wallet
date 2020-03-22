0af93599e5a5e4d495b39d063435d9d82712f290
Mikunj <mikunj@live.com.au>
2020-03-23 10:44:00 +1100

    Add lns record display

---
+++ src/components/lns_record_list.vue
@@ -0,0 +1,117 @@
+<template>
+  <div v-if="records.length > 0" class="lns-record-list">
+    <q-list link no-border :dark="theme == 'dark'" class="loki-list">
+      <q-item v-for="record in records" :key="record.name_hash" class="loki-list-item" :class="bindClass(record)">
+        <q-item-side class="type">
+          <q-icon :name="isLocked(record) ? 'lock' : 'lock_open'" size="24px" />
+        </q-item-side>
+        <q-item-main class="main">
+          <q-item-tile label :class="bindClass(record)">
+            {{ isLocked(record) ? record.name_hash : record.name }}
+          </q-item-tile>
+          <q-item-tile v-if="!isLocked(record)" sublabel>{{ record.value }}</q-item-tile>
+        </q-item-main>
+        <q-item-side right class="height">{{ record.register_height | blockHeight }}</q-item-side>
+
+        <q-context-menu v-if="!isLocked(record)">
+          <q-list link separator style="min-width: 150px; max-height: 300px;">
+            <q-item v-close-overlay @click.native="copyName(record, $event)">
+              <q-item-main :label="$t('menuItems.copyName')" />
+            </q-item>
+
+            <q-item v-close-overlay @click.native="copyValue(record, $event)">
+              <q-item-main :label="record | copyValue" />
+            </q-item>
+          </q-list>
+        </q-context-menu>
+      </q-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+const { clipboard } = require("electron");
+import { mapState } from "vuex";
+import { i18n } from "plugins/i18n";
+
+export default {
+  name: "LNSRecordList",
+  filters: {
+    blockHeight(value) {
+      const heightString = i18n.t("strings.blockHeight");
+      return `${heightString}: ${value}`;
+    },
+    copyValue(record) {
+      if (record.type === "session") {
+        return i18n.t("menuItems.copySessionId");
+      }
+      return i18n.t("menuItems.copyAddress");
+    }
+  },
+  computed: mapState({
+    theme: state => state.gateway.app.config.appearance.theme,
+    records(state) {
+      const records = state.gateway.wallet.lnsRecords;
+      // Sort the records by decrypted ones first, followed by non-decrypted
+      return records.sort((a, b) => {
+        if (a.name && !b.name) {
+          return -1;
+        } else if (b.name && !a.name) {
+          return 1;
+        } else if (a.name && b.name) {
+          return a.name.localeCompare(b.name);
+        }
+        return a.name_hash.localeCompare(b.name_hash);
+      });
+    }
+  }),
+  methods: {
+    isLocked(record) {
+      return !record.name || !record.value;
+    },
+    blurEventButton(event) {
+      for (let i = 0; i < event.path.length; i++) {
+        if (event.path[i].tagName == "BUTTON") {
+          event.path[i].blur();
+          break;
+        }
+      }
+    },
+    bindClass(record) {
+      return [this.isLocked(record) ? "locked" : "unlocked"];
+    },
+    copyName(record, event) {
+      this.copy(record.name, event, this.$t("notification.positive.nameCopied"));
+    },
+    copyValue(record, event) {
+      let message = this.$t("notification.positive.addressCopied");
+      if (record.type === "session") {
+        message = this.$t("notification.positive.sessionIdCopied");
+      }
+      this.copy(record.value, event, message);
+    },
+    copy(value, event, message) {
+      event.stopPropagation();
+      this.blurEventButton(event);
+      if (!value) return;
+      clipboard.writeText(value.trim());
+      this.$q.notify({
+        type: "positive",
+        timeout: 2000,
+        message
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.lns-record-list {
+  .height {
+    font-size: 0.9em;
+  }
+  .q-item.locked {
+    cursor: default;
+  }
+}
+</style>
--- src/pages/wallet/lns.vue
+++ src/pages/wallet/lns.vue
@@ -1,15 +1,18 @@
 <template>
   <q-page class="lns-page">
     <LNSPurchase />
+    <LNSRecordList />
   </q-page>
 </template>
 
 <script>
 import LNSPurchase from "components/lns_purchase";
+import LNSRecordList from "components/lns_record_list";
 
 export default {
   components: {
-    LNSPurchase
+    LNSPurchase,
+    LNSRecordList
   }
 };
 </script>
